537b1a250038398153ae7fa5c0b27a38c8dc2da9
lovesitecore <sebastian.winter@sitecore.com>
2022-06-09 16:00:56 +0200

    Initial creation of profile and theme. Adding Content to Readme.md

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,125 @@
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+#Set-PoshPrompt -Theme free-ukraine
+oh-my-posh --init --shell pwsh --config C:/Users/sebw/AppData/Local/oh-my-posh/themes/sitecore.omp.json | Invoke-Expression
+
+# Add PSReadLine
+# https://github.com/PowerShell/PSReadLine
+# https://devblogs.microsoft.com/powershell/announcing-psreadline-2-1-with-predictive-intellisense/
+# https://docs.microsoft.com/en-us/powershell/module/psreadline/set-psreadlineoption?view=powershell-7.2
+#--------------------
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# Custom styling of the PSReadLine ListView
+Set-PSReadLineOption -Colors @{
+  ListPrediction = '#333378'
+  Emphasis = '#02999A'
+}
+
+#Get-PSReadLineOption
+#Set-PSReadLineOption -Colors @{
+#  ListPredictionColor = 'Blue'
+#  ListPredictionSelectedColor = 'Purple'
+#  Command            = 'Magenta'
+#  Comment          = ''
+#  ContinuationPrompt = ''
+#  DefaultToken       = ''
+#  Emphasis           = ''
+#  Error              = ''
+#  InlinePrediction   = ''
+#  Keyword            = ''
+#  ListPrediction     = ''
+#  ListPredictionSelected = ''
+#  Selection          = ''
+#  String             = ''
+#  Number             = 'DarkGray'
+#  Member             = 'DarkGray'
+#  Operator           = 'DarkGray'
+#  Type               = 'DarkGray'
+#  Variable           = 'DarkGreen'
+#  Parameter          = 'DarkGreen'
+#  ContinuationPrompt = 'DarkGray'
+#  Default            = 'DarkGray'
+#}
+
+# Add support for dotnet suggest shell start
+#-------------------------------------------
+
+function Add-Autocomplete {
+    param (
+        [string[]] $arguments,
+        [string[]] $commandName,
+        [int] $cursorPosition,
+        [System.Management.Automation.Language.CommandAst] $commandAst
+    )
+    $firstELement = $commandAst.CommandElements[0];
+    $commandToExecute = "$firstELement $commandName [suggest:$cursorPosition] ""$arguments"""
+    $listOfSuggestions = Invoke-Expression $commandToExecute
+   
+    $lastArgument = $arguments.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries) | Select-Object -Last 1
+
+ 
+
+    $resultArray = @();
+       
+    if ($lastArgument) {
+        $listOfSuggestions | ForEach-Object {
+            if ($_.StartsWith($lastArgument)) {
+                $resultArray += $_
+            }
+        }
+    }
+
+ 
+
+    if ($resultArray.Count -eq 0) {
+        $resultArray = $listOfSuggestions
+    }
+
+ 
+
+    $resultArray | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+ 
+
+# local Tool autocomplete
+Register-ArgumentCompleter -Native -CommandName "dotnet" -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+ 
+
+        $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
+        $commandName = "sitecore"
+        if ($arguments.Contains($commandName)) {
+            Add-Autocomplete -arguments $arguments -commandName $commandName -commandAst $commandAst -cursorPosition $cursorPosition
+        }
+
+ 
+
+        # Root command autocomplete
+        if ($commandName.StartsWith($commandAst.CommandElements[1])) {
+            [System.Management.Automation.CompletionResult]::new($commandName, $commandName, 'ParameterValue', $commandName)
+        }
+
+ 
+
+}    
+
+ 
+
+# Global Tool autocomplete
+Register-ArgumentCompleter -Native -CommandName  "sitecore" -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+ 
+
+    $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
+    Add-Autocomplete -arguments $arguments -commandAst $commandAst -cursorPosition $cursorPosition
+}
--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Windows-Terminal-DevEx-improvements
 Sitecore branded Windows Termial and VS Code Theme and Profile to unlock helpful tooling for working with XM Cloud such as autocompletion, command history list and directory improvements. However this Theme and profile is Sitecore independent and can be used with any terminal related work.
+
+# Preconditions
+
+## Install Windows Terminal
+- Go to Microsoft Store and search for Windows Terminal
+- Hit Install Button
+- Launch Windows Terminal
+
+## Install Powershell Version 7.7.2
+- Go to Microsoft Store and search for Powershell
+- Hit Install Button
+- Restart Windows Terminal (close and open)
+
+## Make Powershell 7.7.2 the standard shell
+- Open Settings
+- In Startup Selected PowerShell as your default profile
+
+## Install Oh My Posh
+
+- In Terminal run: 
+  Install-Module posh-git -Scope CurrentUser
+- Confirm that you trust the repository with "a"
+
+- Run: Install-Module oh-my-posh -Scope CurrentUser
+- Confirm that you trust the repository with "a"
+
+- Run: Install-Module -Name PSReadLine -AllowPrerelease -Force
+
+- Run: Install-Module -Name Terminal-Icons -Repository PSGallery
+
+### Test Oh My Posh
+
+- In Terminal run: Set-PoshPrompt
+
+## Install Nerd Fonts
+- Go to Nerdfonts.com and
+- Download Caskaydia Cove Nerd Font
+- Install Fonts in zip file
+
+- In Windows Terminal
+- Go to Settings
+- Go To Appearance
+- Select Font face "CaskaydiaCove NF"
+
+### Test
+- In Terminal glyphs should be shown now
+
+## Get available Posh Themes (optional)
+That step will download available Themes
+
+- In Terminal run:
+  Get-PoshThemes
+
+## Switch Theme (optional)
+- In Terminal run: 
+  Set-PoshPrompt -Theme free-ukraine
+
+# Use Sitecore branded theme and profile
+
+## Change the Theme 
+- Download sitecore.omp.json
+- Copy file to that path: C:\Users\<username>\AppData\Local\oh-my-posh\themes\
+
+## Change the Profile
+- Download "Microsoft.PowerShell_profile.ps1" from Repository
+- Copy file to that path: C:\Users\<username>\Documents\PowerShell\Microsoft.Powerhsell_profile.ps1 and overwrite the extisting (if any)
+
+
